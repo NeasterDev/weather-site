4f9609fd1cd001e2cd958986861553efd9862a53
Nicholas Easter <nicholas.easter17@gmail.com>
2022-11-03 00:04:41 -0700

    got the data i need from openweathermap api and printed some of it on the screen and formatted the dates

--- src/components/CurrentWeatherCard.js
+++ src/components/CurrentWeatherCard.js
@@ -1,22 +1,40 @@
 // This card will be used to display the current weather data
 
-export const CurrentWeatherCard = ({ date, currentTemp, forecastArray, feelsLike }) => {
+export const CurrentWeatherCard = ({
+  date,
+  currentTemp,
+  forecastArray,
+  feelsLike,
+}) => {
+  // these are the format options for the localeDateString
+  const options = {
+    month: "short", // Nov
+    day: "numeric", // 00
+    year: "numeric", // 0000
+  };
 
-    console.log(date);
-    let temp_date = new Date(date);
-    return (
-        <div>
-            <div>Current Date: {date.toLocaleDateString("en-US", {month: "short", day: "numeric", year: "numeric"})}</div>
-            <div>Current temp: {currentTemp}</div>
-            <div>Current feelslike: {feelsLike}</div>
-            <div>Current forecast: {forecastArray[0].clouds}</div>
-            {forecastArray.map((weather, index) => {
-                temp_date.setDate(temp_date.getDate() + index);
-                return <div key={index}>
-                    <div>Date: {temp_date.toLocaleDateString("en-US", {month: "short", day: "numeric", year: "numeric"})}</div>
-                    <div> High/Low: {weather.temp.min}/{weather.temp.max} </div>
-                </div>
-            })}
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>Current Date: {date.toLocaleDateString("en-US", options)}</div>
+      <div>Current temp: {currentTemp}</div>
+      <div>Current feelslike: {feelsLike}</div>
+      <div>Current forecast: {forecastArray[0].clouds}</div>
+      <br />
+      <hr />
+      <br />
+      {forecastArray.map((weather, index) => {
+        return (
+          <div key={index}>
+            <div>
+              Date:
+              {new Date(weather.dt * 1000).toLocaleDateString("en-US", options)}
+            </div>
+            <div>
+              High/Low: {weather.temp.min}/{weather.temp.max}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
