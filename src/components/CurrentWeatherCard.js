76c4dbbd5c9473ae52bd187f642ae64e44b6d013
Nicholas Easter <nicholas.easter17@gmail.com>
2022-11-03 23:06:32 -0700

    pushing changes to desktop

--- src/components/CurrentWeatherCard.js
+++ src/components/CurrentWeatherCard.js
@@ -3,38 +3,15 @@
 export const CurrentWeatherCard = ({
   date,
   currentTemp,
-  forecastArray,
   feelsLike,
+  options
 }) => {
-  // these are the format options for the localeDateString
-  const options = {
-    month: "short", // Nov
-    day: "numeric", // 00
-    year: "numeric", // 0000
-  };
 
   return (
     <div>
       <div>Current Date: {date.toLocaleDateString("en-US", options)}</div>
       <div>Current temp: {currentTemp}</div>
       <div>Current feelslike: {feelsLike}</div>
-      <div>Current forecast: {forecastArray[0].clouds}</div>
-      <br />
-      <hr />
-      <br />
-      {forecastArray.map((weather, index) => {
-        return (
-          <div key={index}>
-            <div>
-              Date:
-              {new Date(weather.dt * 1000).toLocaleDateString("en-US", options)}
-            </div>
-            <div>
-              High/Low: {weather.temp.min}/{weather.temp.max}
-            </div>
-          </div>
-        );
-      })}
     </div>
   );
 };
--- src/components/DailyWeatherCard.js
+++ src/components/DailyWeatherCard.js
@@ -1,1 +1,29 @@
 // this weather card will be used for multi-day forecasts
+
+export const DailyWeatherCard = ({ forecastArray }) => {
+
+    // these are the format options for the localeDateString
+    const options = {
+        month: "short", // Nov
+        day: "numeric", // 00
+        year: "numeric", // 0000
+    };
+    
+    return (
+        <div>
+            {forecastArray.map((weather, index) => {
+                return (
+                    <div key={index}>
+                        <div>
+                            Date:
+                            {new Date(weather.dt * 1000).toLocaleDateString("en-US", options)}
+                        </div>
+                        <div>
+                            High/Low: {weather.temp.min}/{weather.temp.max}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
